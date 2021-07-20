37b88a5e731b9b10a542b0c900f3459aca99b018
Mike Morton <mmorton7295@gmail.com>
2021-07-20 09:16:57 -0500

    creates index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Simple Movies App</h1>
+    <hr />
+    <div v-for="movie in movies">
+      <h3> {{ movie.title }} </h3>
+      <p> {{ movie.year }} </p>
+      <p><strong>Directed by: </strong>{{ movie.director }}</p>
+      <p> <strong>Plot: </strong> {{ movie.plot }} </p>
+      <p>
+        <span><strong>Genres: </strong>|</span>
+        <span v-for="genre in movie.genre_names"> {{ genre }} |</span>
+        
+
+      </p>
+      <p>
+      <span><strong>Actors:</strong></span>
+      <span v-for="actor in movie.actors"> {{ actor.first_name }} {{ actor.last_name }}, </span>
+      </p>
+      <hr />
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      movies: [],
+    };
+  },
+  created: function () {
+    this.moviesIndex();
+  },
+  methods: {
+    moviesIndex: function () {
+      axios.get("/movies").then((response) => {
+        this.movies = response.data;
+        console.log(this.movies);
+      });
+    },
+  },
+};
 </script>
